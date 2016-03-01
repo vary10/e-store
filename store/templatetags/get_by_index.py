afdcbd296e194a17f83266b3a46abac2f6e3c351
Valery Girkin <vary10@gmail.com>
2016-03-01 22:25:04 +0300

    beautified html mk2

--- store/templatetags/get_by_index.py
+++ store/templatetags/get_by_index.py
@@ -8,6 +8,8 @@ def get_by_index(value, arg):
     try:
         if i < 0:
             i += len(value)
+        if i >= len(value):
+            i = len(value) - 1
         return [value[i]]
     except:
         print("NO SUCH INDEX")
